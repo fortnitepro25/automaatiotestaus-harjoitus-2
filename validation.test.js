3e654005c10f2205c99a0e0ad886ad9180d1b488
fortnitepro25 <s2401240@edu.bc.fi>
2026-09-11 12:14:21 +0300

    Tehtävä 5 valmis

--- validation.test.js
+++ validation.test.js
@@ -41,7 +41,15 @@ test("validatePassword hyväksyy vähintään 8 merkkiä pitkän salasanan", () 
 // Tehtävä 4
 // Testataan, että ikä 18 hyväksytään.
 test("validateAge hyväksyy iän 18", () => {
-const result = validateAge(18);
+  const result = validateAge(18);
 
-assert.strictEqual(result, true);
+  assert.strictEqual(result, true);
+});
+
+// Tehtävä 5
+// Testataan, että ikä 15 hylätään.
+test("validateAge hylkää iän 15", () => {
+  const result = validateAge(15);
+
+  assert.strictEqual(result, false);
 });
