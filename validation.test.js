bfca2121a4f6cdc3a4fd4fb77c69703b89563ed9
fortnitepro25 <s2401240@edu.bc.fi>
2026-09-11 11:55:09 +0300

    Tehtävä 3 valmis

--- validation.test.js
+++ validation.test.js
@@ -1,12 +1,7 @@
 import { test } from "node:test";
 import assert from "node:assert";
 
-import {
-  validateEmail,
-  validatePassword,
-  validateAge
-} from "./validation.js";
-
+import { validateEmail, validatePassword, validateAge } from "./validation.js";
 
 test("validateEmail hyväksyy tavallisen sähköpostiosoitteen", () => {
   const result = validateEmail("opiskelija@example.com");
@@ -30,7 +25,15 @@ test("validatePassword hylkää liian lyhyen salasanan", () => {
 // Tehtävä 2
 // Testataan että liian lyhyt salasana hylätään.
 test("validatePassword hylkää liian lyhyen salasanan", () => {
-const result = validatePassword("sala123");
+  const result = validatePassword("sala123");
 
-assert.strictEqual(result, false);
+  assert.strictEqual(result, false);
+});
+
+// Tehtävä 3
+// Testataan että vähintään 8 merkkiä pitkä salasana hyväksytään.
+test("validatePassword hyväksyy vähintään 8 merkkiä pitkän salasanan", () => {
+  const result = validatePassword("salasana");
+
+  assert.strictEqual(result, true);
 });
